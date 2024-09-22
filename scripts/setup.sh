8e2d550ba784aedcf967cecface206aa59f5b44b
Ubuntu <ubuntu@ip-172-31-2-145.ec2.internal>
2024-09-22 05:08:47 +0000

    making minor changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,12 @@ script_url="https://raw.githubusercontent.com/jonwang22/microblog_VPC_deployment
 start_script="source $download_path"
 
 # SSHing into AppServer
-ssh -i "$ssh_key" "$login_name@$application_server_IP" << EOF
-    curl -L -o $download_path $script_url && $start_script
-EOF
+
+# Trying to reduce output with this
+ssh -i "$ssh_key" "$login_name@$application_server_IP" "curl -L -o $download_path $script_url 2>/dev/null && chmod 755 $download_path && $start_script && rm $download_path"
+
+
+# Clean way to run commands via SSH. Issue with this is the SSH login terminal output shows up along with the output from the commands we want to run.
+#ssh -i "$ssh_key" "$login_name@$application_server_IP" << EOF 2>/dev/null
+#    curl -L -o $download_path $script_url 2>/dev/null && chmod 755 $download_path && $start_script && rm $download_path
+#EOF
