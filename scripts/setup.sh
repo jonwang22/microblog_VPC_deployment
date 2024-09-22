208a6e7756d7d5a783a47c2cede2b8f09ea7c77c
Ubuntu <ubuntu@ip-172-31-2-145.ec2.internal>
2024-09-22 04:39:09 +0000

    replacing private IP with env variable

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-application_server_IP="10.0.5.221"
+appilication_server_IP="$APPLICATION_SERVER_IP" # <----$APPLICATION_SERVER_IP is set within .bashrc on the webserver as an environment variable
+download_path="/home/ubuntu/start_app.sh"
+login_name="ubuntu"
+ssh_key="/home/ubuntu/.ssh/AppWL4.pem"
+script_url="https://raw.githubusercontent.com/jonwang22/microblog_VPC_deployment/refs/heads/main/scripts/start_app.sh"
+start_script="source $download_path"
 
 # SSHing into AppServer
-ssh -i .ssh/AppWL4.pem ubuntu@10.0.5.221 'source /home/ubuntu/start_app.sh' 
+ssh -i "$ssh_key" "$login_name@$application_server_IP" << EOF
+    curl -L -o $download_path $script_url && $start_script
+EOF
