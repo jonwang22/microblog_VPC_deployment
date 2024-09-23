e0172b3c1afadb5a5919ef78459327927a2f78f3
Ubuntu <ubuntu@ip-10-0-2-152.ec2.internal>
2024-09-23 01:01:17 +0000

    fixing setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,27 +6,34 @@ repo_path="/home/ubuntu/microblog_VPC_deployment"
 login_name="ubuntu"
 ssh_key="/home/ubuntu/.ssh/AppWL4.pem"
 script_url="https://raw.githubusercontent.com/jonwang22/microblog_VPC_deployment/refs/heads/main/scripts/start_app.sh"
-start_script="source $file_path"
 
 # SSHing into AppServer
 
 # Trying to reduce output with this
-ssh -i "$ssh_key" "$login_name@$application_server_IP" << EOF 2>/dev/null
+ssh -i "$ssh_key" "$login_name@$application_server_IP" << EOF
 if  pgrep -f gunicorn > /dev/null; then
-                echo "Gunicorn is running, cleaning environment."
-                pkill gunicorn
-        else
-                echo "Gunicorn is currently not running. Environment is ready."
-        fi
+        echo "Gunicorn is running, cleaning environment."
+        pkill gunicorn
+	echo "Gunicorn process killed."
+else
+        echo "Gunicorn is currently not running. Environment is ready."
+fi
 
 if [[ -d "$repo_path" ]] && [[ -f "$file_path" ]]; then
 	echo "Repository and File already exist."
 	echo "Deleting existing local repo and file."
-	rm -rf "$repo_path"
-	rm "$file_path"
-	curl -L -o "$file_path" "$script_url" 2>/dev/null && chmod 755 "$file_path" && "$start_script"
+	rm -rf $repo_path
+	rm $file_path
+	curl -L -o $file_path $script_url 2>/dev/null
+	chmod 755 $file_path
+	echo "Downloaded script and set permissions."
+	source $file_path
 else
-	curl -L -o "$file_path" "$script_url" 2>/dev/null && chmod 755 "$file_path" && "$start_script"
+	echo "Downloading and starting the script."
+	curl -L -o $file_path $script_url 2>/dev/null
+	chmod 755 $file_path
+	echo "Downloaded script and set permissions."
+	source $file_path
 fi
 EOF
 
